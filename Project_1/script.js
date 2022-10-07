2e2864bd5353c31bad3ec170453033a693eecd1e
HalilSacirovic <halilsacirovic03@gmail.com>
2022-10-07 22:36:53 +0200

    First JavaScript lecture

---
+++ Project_1/script.js
@@ -0,0 +1,31 @@
+let js = "amazing";
+console.log (40+8+10+12);
+
+console.log ("Jonas");
+console.log (23)
+
+let firstName = "Jonas";
+console.log (firstName);
+console.log (firstName);
+console.log (firstName);
+
+
+//  We can't write code name with a number example
+
+// let 3years:3; We can not do that like this 
+
+
+// We can start with $ or _ 
+
+// exaple : $function halil_sacirovic or like that 
+
+
+let myFirstJob = "Programmer"
+let myCurrentJob = "Teacher"
+
+// This is the better way to write like that thern like this 
+
+let job1 = "Programmer"
+let job2 = "Teacher"
+
+console.log (myFirstJob, "is the better job than",myCurrentJob)
