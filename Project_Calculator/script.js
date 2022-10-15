b4755084b29c979429ace7b0e58f7bbfbafbabd6
HalilSacirovic <halilsacirovic03@gmail.com>
2022-10-15 22:11:34 +0200

    Calculator design

---
+++ Project_Calculator/script.js
@@ -0,0 +1,62 @@
+var a,b,c,d,e,f,g,h,j,k;
+a=1;
+b=2;
+c=3;
+d=4;
+e=5;
+f=6;
+g=7;
+h=8;
+j=9;
+k=0;
+
+document.getElementById("one").addEventListener("click", function () {
+    document.getElementById("result").textContent = a;
+  });
+  
+document.getElementById("two").addEventListener("click", function () {
+    document.getElementById("result").textContent = b;
+  });
+  
+document.getElementById("tree").addEventListener("click", function () {
+    document.getElementById("result").textContent = c;
+  });
+  
+document.getElementById("four").addEventListener("click", function () {
+    document.getElementById("result").textContent = d;
+  });
+  
+document.getElementById("five").addEventListener("click", function () {
+    document.getElementById("result").textContent = e;
+  });
+  
+document.getElementById("six").addEventListener("click", function () {
+    document.getElementById("result").textContent = f;
+  });
+  
+document.getElementById("seven").addEventListener("click", function () {
+    document.getElementById("result").textContent = g;
+  });
+  
+document.getElementById("eight").addEventListener("click", function () {
+    document.getElementById("result").textContent = h;
+  });
+  
+document.getElementById("nine").addEventListener("click", function () {
+    document.getElementById("result").textContent = j;
+  });
+  
+document.getElementById("zero").addEventListener("click", function () {
+    document.getElementById("result").textContent = k;
+  });
+  
+
+
+document.getElementById("delete").addEventListener("click", function () {
+    document.getElementById("result").textContent = this.nonce;
+  });
+
+document.getElementById("equal").addEventListener("click", function () {
+    document.getElementById("result").textContent = this.nonce;
+  });
+  
