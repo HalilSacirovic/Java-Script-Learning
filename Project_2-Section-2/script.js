1b183d3fac39edf38cf640348dcddb345b8ff33c
HalilSacirovic <halilsacirovic03@gmail.com>
2022-11-02 18:46:05 +0100

    Functions

---
+++ Project_2-Section-2/script.js
@@ -0,0 +1,63 @@
+'use strict'; 
+// This help us to show us what is the problem in our code
+// exaple if we didnt name our variable fully true this will show us what is the problem with line of code exaple is done there
+
+
+
+/* 
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest ) hasDriversLicence = true;
+if (hasDriversLicence) console.log ("I can drive a car");
+*/ 
+
+
+// I write this variable wrong on purpose because You can see that will show you in 
+// a console what is the wrong with code or variable in this case
+// I named variable wrong I forgot to put "s" and becasue of that
+// this if statement didnt work because it's not true;
+// but When I put "s " like the real name of variable  this code will
+// work because I changed hasDriversLicence from "FALSE" to "TRUE"
+// If you can not manage with this go to the course SECTION 3 32 Lecture;
+
+
+
+
+// LECTURE 33 FUNCTIONS
+
+/* 
+
+
+function logger () {
+    console.log('My name is Halil');
+}
+// calling / running / invoking function
+
+
+logger ();
+logger ();
+logger ();
+
+
+function fruitProcessor(apples, oranges) {
+    console.log (apples,oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+
+}
+
+ const appleJuice = fruitProcessor(5, 0);
+console.log( appleJuice);
+console.log (fruitProcessor(5,0 ));
+
+const appleOrangeJuice = fruitProcessor (2, 4 );
+console.log (appleOrangeJuice);
+
+*/
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+
