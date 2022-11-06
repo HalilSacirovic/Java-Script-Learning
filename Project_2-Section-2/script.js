b51c85b41c35ece9ff368343ffe695cb0c3e4957
HalilSacirovic <halilsacirovic03@gmail.com>
2022-11-06 19:09:27 +0100

    Array & Functions; Codding Challenge #2; Object; Dot Vs Brackets;

--- Project_2-Section-2/script.js
+++ Project_2-Section-2/script.js
@@ -187,7 +187,6 @@ Hints:
 § To check if number A is at least double number B, check for A >= 2 * B. 
 Apply this to the team's average scores 
 
-*/
 
 
 const calcAverage = (a,b,c) => (a+b+c) / 3;
@@ -201,18 +200,223 @@ const scoreKoalas = calcAverage(23,34,27);
 console.log(scoreDolphins,scoreKoalas);
 
 const checkWinner = function (avgDolhins, avgKoalas) {
-
-if(avgDolhins >= 2*avgKoalas) {
-    console.log (`Dolphins win the Trophie (${avgDolhins} vs ${avgKoalas} )`);
-}
-
-else if (avgKoalas >= 2*avgDolhins) {
-    console.log (`Koalas win the Trophie (${avgKoalas} vs ${avgDolhins} )`);
-}
-else {
-    console.log("Nobody win the trophy");
-}
+    
+    if(avgDolhins >= 2*avgKoalas) {
+        console.log (`Dolphins win the Trophie (${avgDolhins} vs ${avgKoalas} )`);
+    }
+    
+    else if (avgKoalas >= 2*avgDolhins) {
+        console.log (`Koalas win the Trophie (${avgKoalas} vs ${avgDolhins} )`);
+    }
+    else {
+        console.log("Nobody win the trophy");
+    }
     
 }
 
 checkWinner(scoreDolphins,scoreKoalas)
+
+
+
+
+// LECTURE 39  ===== ARRAY
+
+
+const friend1 = 'Michael'
+const friend2 = 'Steven'
+const friend3 = 'Peter'
+
+// INSTEAD OF THAT WE CAN DO LIKE THIS
+
+const friends = ['Michael','Steven','Peter'];
+console.log(friends);
+
+// const years = new Array (1991,1984,2008,2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+
+console.log(friends.length);
+console.log(friends[friends.length-1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Halil';
+// const halil = ['Halil','Sacirovic',2037-1991];
+// We can  can do that but
+
+const halil = [firstName,'Sacirovic',2037-1991,'Programmer',friends];
+
+console.log(halil);
+
+
+// const
+
+
+const calcAge =function (birthYear){
+    return 2037- birthYear;
+    
+}
+
+const years = [1990,1967,2002,2010,2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log( age1,age2,age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length-1])]
+
+console.log(ages)
+
+
+
+
+///////////////////////////////////////
+// LECTURE 40 ARRAY FUNCTIONS
+
+
+
+
+// Basic Array Operations (Methods)
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // ADD TO THE LAST PLACE
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // add AT THE FIRST PLACE
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+}
+*/
+
+///////////////////////////////////////
+
+// LECTURE 41
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+
+/////////////////////////////////////////////////////////////////////
+
+const calcTip = function (bill) {
+    return bill <=300 && bill>= 50 ? bill* 0.15 : bill * 0.2;
+    
+}
+
+const bills = [125,555,44];
+const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+console.log(bills,tips);
+
+
+
+/////////////////////////////////////////////////////////////
+
+PITAJ PROFESORA DA TI OBJASNI OVO GORE 
+
+
+
+//////////////////////////////////////////////////////////////
+
+
+const total = [bills[0]+tips[0]];
+console.log (total)
+
+
+
+*/
+
+
+
+// LECTURE 42-43  Objects  and Dot vs Bracket
+
+
+const jonas = {
+    firstName:'Jonas',
+    lastName:'Schmedtman',
+    age:2037-1991,
+    job:'Programmer',
+    friends:['Michael','Peter','Steven']
+}
+
+
+console.log(jonas);
+console.log(jonas.lastName); // DOT OPERATOR 
+console.log(jonas['lastName']); // BRACKETS OPERATOR
+
+
+const nameKey = 'Name';
+console.log(jonas['first'+ nameKey]);
+console.log(jonas['last'+ nameKey]);
+
+
+const interestedIn = prompt('What do you want to know about Jonas?Choose between firstName,lastName,age,job and friends');
+console.log(jonas[interestedIn]); // LIKE THIS 
+
+// console.log(jonas.interestedIn);  // WE CAN NOT DO LIKE THIS INSTEAD OF WE DO LIKE THIS UP THERE
+
+
+// If someone or I put something that is not in jonas Value exaple:he put that he want to know his location
+// that is not gonna work, and it will show you in console that is undefined 
+
+// So if we want to change that undefined to write something else we use IF/ ELSE STATEMENT
+
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]); // LIKE THIS 
+    
+}
+else {
+    console.log ("We dont have information about that.Choose between firstName,lastName,age,job and friends")
+}
+
+
+// SO this happends because undefined is FALSE value, so IF Jonas[InternedIN] is true that will
+// write you what you want if we have it 
+// But if we dont have it or if statemnt is not true that will go to the 
+// ELSE STATEMENT and Do what is in there.
+
+
+// NOW IF we want to add something more in there we can do LIKE THIS DOWN
+
+jonas.location = 'Portugal'; // DOT METHOD 
+jonas['twitter']= '@jonasschmedtmen'; // BRACKET METHOD
+console.log(jonas);
+
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends,and his best friend is called ${jonas.friends[0]}`);
