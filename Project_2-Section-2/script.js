9b7a647fce60da2377a35503e60ff0c066eeaf19
HalilSacirovic <halilsacirovic03@gmail.com>
2022-11-07 19:04:22 +0100

    Coding Challenge #3

--- Project_2-Section-2/script.js
+++ Project_2-Section-2/script.js
@@ -359,7 +359,6 @@ console.log (total)
 
 
 
-*/
 
 
 
@@ -420,3 +419,69 @@ console.log(jonas);
 
 
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends,and his best friend is called ${jonas.friends[0]}`);
+
+
+
+
+
+
+// LECTURE 45 CODING CHALLENGE #3
+
+
+
+
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
+implement the calculations! Remember: BMI = mass / height ** 2 = mass 
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+
+1. For each of them, create an object with properties for their full name, mass, and 
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same 
+    method on both objects). Store the BMI value to a property, and also return it 
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the 
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m 
+tall.
+GOOD LUCK 
+
+
+*/
+
+
+const mark = {
+    firstName:'Mark',
+    lastName:'Miller',
+    height: 1.69,
+    mass:78,
+    calcBMI: function (){
+        this.bmi = this.mass / this.height ** 2 ;
+        return this.bmi;
+    }
+}
+
+const john = {
+    firstName:'John',
+    lastName:'Smith',
+    height: 1.95,
+    mass:92,
+    calcBMI: function (){
+        this.bmi = this.mass / this.height ** 2 ;
+        return this.bmi;
+    }
+}
+
+mark.calcBMI();
+john.calcBMI();
+console.log (mark.bmi, john.bmi);
+
+
+if(mark.bmi > john.bmi) {
+    console.log( `${mark.firstName}'s BMI (${mark.bmi}
+        is higher than ${john.firstName}'s bmi (${john.bmi}))`)
+}
+else if (john.bmi > mark.bmi) {
+        console.log( `${john.firstName}'s bmi (${john.bmi}
+        is higher than ${mark.firstName}'s bmi (${mark.bmi}))`)
+}
