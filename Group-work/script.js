7128dab836e033b2d9a48f9a8d26ba5027d45eeb
HalilSacirovic <halilsacirovic03@gmail.com>
2022-11-21 20:55:44 +0100

    group work

---
+++ Group-work/script.js
@@ -0,0 +1,15 @@
+function gimme (triplet) {
+
+  if(triplet[0]>triplet[1]){
+    
+    if(triplet[2]<triplet[1]){
+     return triplet[1];
+      
+    }
+  }
+
+
+
+}
+
+console.log(gimme([4,5,12]))
