204dd3c59341a670f774d970035120a3eabd1abc
HalilSacirovic <halilsacirovic03@gmail.com>
2022-11-23 20:55:23 +0100

    Gene

--- Group-work/script.js
+++ Group-work/script.js
@@ -1,4 +1,4 @@
-function gimme (triplet) {
+/*function gimme (triplet) {
 
   if(triplet[0]>triplet[1]){
     
@@ -13,3 +13,53 @@ function gimme (triplet) {
 }
 
 console.log(gimme([4,5,12]))
+
+
+
+
+function invert(array) {
+
+  for(i=0;i<array.length;i++){
+    array[i]=array[i]*(-1);
+  }
+  return array;
+}
+
+
+
+function countPositivesSumNegatives(input) {
+  var sum=0;
+  var br=0;
+  
+  for (var i=0;i<input.length;i++){
+    if (input[i]>0 ) {
+      br++
+    }
+    if(input[i]<0){
+      sum=sum+input[i]
+    }
+  }
+  return [br,sum]
+}
+
+*/
+function sumOfDifferences(arr) {
+  var sum=0;
+    
+    for (var i = 0; i <= arr.length - 2; i++) {
+      for (var j = i + 1; j <= arr.length - 1; j++) {
+        if (arr[i] < arr[j]) {
+          var pom = arr[i];
+          arr[i] = arr[j];
+          arr[j] = pom;
+        }
+      }
+    }
+  
+  for(var k=0;k<arr.length -1;k++){
+    sum=sum+(arr[k]-arr[k+1]);
+  }
+  
+  return sum;
+  }
+  
