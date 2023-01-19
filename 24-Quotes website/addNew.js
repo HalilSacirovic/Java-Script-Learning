ad07414d8d713d654380a958078ba21dc85950a2
HalilSacirovic <halilsacirovic03@gmail.com>
2023-01-19 20:05:04 +0100

    POST Method

--- 24-Quotes website/addNew.js
+++ 24-Quotes website/addNew.js
@@ -1,14 +1,25 @@
+var arr= []
+
+
 document.getElementById("addnew").addEventListener("click",function(){
     var text = document.getElementById("quoteText").value;
     var author = document.getElementById("quoteAuthor").value;
     var source = document.getElementById("quoteSource").value;
 
+
+    
     var newQuote = {
         quoteText:text,
         quoteAuthor:author,
         qutoteSource:source,
     };
+    
+    
+    document.getElementById("quoteText").value = "";
+    document.getElementById("quoteAuthor").value = "";
+    document.getElementById("quoteSource").value = "";
 
+    arr.push(newQuote)
 
     fetch("https://js-course-server.onrender.com/quotes/add-quote",{
         method:"POST",
@@ -20,6 +31,14 @@ document.getElementById("addnew").addEventListener("click",function(){
     }).then(function(res){
         return res.json();
     }).then(function(data){
-        console.log("The quote is created",data);
+        var parent = document.getElementById("test");
+
+
+        for(var i = 0 ; i<arr.length;i++){
+            
+            var child = document.createElement("li");
+            child.textContent = '"' + arr[i].quoteText + '" ' + "     " + arr[i].quoteAuthor  + "    " + '"' + arr[i].qutoteSource + '"';
+            parent.appendChild(child);
+        }
     })
 })
